e4f408603858b4a08c2d9a0ecb3a2c9d8f649a00
su-su-su-su <suzukikoji0503@gmail.com>
2023-05-06 16:21:41 +0900

    Usersモデルに郵便番号、住所と自己紹介文カラムを追加。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
+  resources :users, only: [:index, :show]
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
