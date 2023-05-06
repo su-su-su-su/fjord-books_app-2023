14c579629f9ce772b3c40db54c5543d29779f886
su-su-su-su <suzukikoji0503@gmail.com>
2023-05-06 22:40:39 +0900

    ユーザーの一覧と詳細関連の処理を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.order(:id).page(params[:page]).per(2).order(:created_at, :id)
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   get 'users/index'
   get 'users/show'
+
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   resources :users, only: [:index, :show]
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
