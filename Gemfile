bb4a358ab469a95932b803108d412101d4f06e9c
su-su-su-su <suzukikoji0503@gmail.com>
2023-05-15 01:00:05 +0900

    パスワード再設定メールの追加

--- Gemfile
+++ Gemfile
@@ -49,6 +49,8 @@ gem 'bootsnap', require: false
 
 gem 'devise'
 
+gem 'letter_opener_web'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
   resources :users, only: [:index, :show]
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
