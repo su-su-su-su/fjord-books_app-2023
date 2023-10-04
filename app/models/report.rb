f5ac630156aebe1b2fba36a2995255594b8ae39f
su-su-su-su <suzukikoji0503@gmail.com>
2023-10-05 02:11:52 +0900

    mention model creation, association using has_many :through
    mentionモデル作成、has_many :throughを使った関連付け

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,11 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :active_mentions, class_name: "Mention", foreign_key: "mentioning_report_id", dependent: :destroy
+  has_many :mentioning_reports, through: :active_mentions, source: :mentioning_report
+  has_many :passive_mentions, class_name: "Mention", foreign_key: "mentioned_report_id", dependent: :destroy
+  has_many :mentioned_reports, through: :passive_mentions, source: :mentioned_report
+
 
   validates :title, presence: true
   validates :content, presence: true
